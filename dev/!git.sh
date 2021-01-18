49e93f9531159dfc827feb0f5def4bef975ba821
Feas2016 <else2016@yandex.ru>
2021-01-18 03:10:53 +0300

    0.3c

--- dev/!git.sh
+++ dev/!git.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 cd /f/Programming/HLL_stats/
-git init
+#git init
+#git remote add master https://github.com/Feas2016/HLL_weapon_stats.git
 git add *
-git show
-git status
-git commit -m "GUI. Read and write mdb file"
-git remote add main https://github.com/Feas2016/HLL_weapon_stats.git
-git push -u main
+#git show
+git status #
+git commit -m "0.3b"
+git push -u master
 git log --name-status
 $SHELL
